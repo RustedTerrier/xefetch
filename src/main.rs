f8c059eca1a3d4ecb920c9632c7275d27c59d6d1
RustedTerrier <RustedTerrier@tutanota.com>
2021-02-10 09:47:26 -0500

    Feat: added kernel, uptime, and made it not illegal

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,20 @@ fn main() {
     v = hme.split('/').collect();
     let user = v[v.len() - 1];
     //Get Host name
+    //MIT License
+
+    //Copyright (c) 2019-2020 The rsfetch contributors
+
+    //Permission is hereby granted, free of charge, to any person obtaining a copy
+    //of this software and associated documentation files (the "Software"), to deal
+    //in the Software without restriction, including without limitation the rights
+    //to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    //copies of the Software, and to permit persons to whom the Software is
+    //furnished to do so, subject to the following conditions:
+    //
+    //The above copyright notice and this permission notice shall be included in all
+    //copies or substantial portions of the Software.
+
     let hst = Command::new("hostname")
         .output()
         .expect("Could not find hostname.");
@@ -48,12 +62,36 @@ fn main() {
         .expect("Error: file /sys/devices/virtual/dmi/id/product_name not found.");
     v = mdl.split('\n').collect();
     let model = v[0].to_string();
+
+    //Kernel
+    //MIT License
+
+    //Copyright (c) 2019-2020 The rsfetch contributors
+
+    //Permission is hereby granted, free of charge, to any person obtaining a copy
+    //of this software and associated documentation files (the "Software"), to deal
+    //in the Software without restriction, including without limitation the rights
+    //to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    //copies of the Software, and to permit persons to whom the Software is
+    //furnished to do so, subject to the following conditions:
+    //
+    //The above copyright notice and this permission notice shall be included in all
+    //copies or substantial portions of the Software.
+    let krnl = Command::new("uname")
+        .arg("-r")
+        .output()
+        .expect("Could not find kernel version.");
+    let kernl = String::from_utf8(krnl.stdout).unwrap().replace("\n", "");
+    let kernel = kernl.clone();
+    let uptime = format_uptime();
     println!(
-        "{}@{}\n\rOS:    {}\n\rHOST:  {}\n\rSHELL: {}\n\r{}██{}██{}██{}██{}██{}██{}██{}██\n{}██{}██{}██{}██{}██{}██{}██{}██{reset}",
+        "{}@{}\n\rOS:     {}\n\rHOST:   {}\n\rKERNEL: {}\n\rUPTIME: {}\n\rSHELL:  {}\n\r{}██{}██{}██{}██{}██{}██{}██{}██\n{}██{}██{}██{}██{}██{}██{}██{}██{reset}",
         user,
         host,
         distro,
         model,
+        kernel,
+        uptime,
         shell,
         black,
         red,
@@ -73,4 +111,37 @@ fn main() {
         lwhite,
         reset = color::Fg(color::Reset),
     );
+}
+
+fn format_uptime() -> String {
+    //MIT License
+
+    //Copyright (c) 2019-2020 The rsfetch contributors
+
+    //Permission is hereby granted, free of charge, to any person obtaining a copy
+    //of this software and associated documentation files (the "Software"), to deal
+    //in the Software without restriction, including without limitation the rights
+    //to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    //copies of the Software, and to permit persons to whom the Software is
+    //furnished to do so, subject to the following conditions:
+    //
+    //The above copyright notice and this permission notice shall be included in all
+    //copies or substantial portions of the Software.
+    let mut sec: &str = &*fs::read_to_string("/proc/uptime").expect("File /proc/uptime not found.");
+    sec = sec.split('.').collect::<Vec<&str>>()[0];
+    let secs = sec.parse::<u64>().unwrap();
+    let days = secs / 60 / 60 / 24;
+    let hours = (secs / 60 / 60) % 24;
+    let minutes = (secs / 60) % 60;
+    let mut uptime = "".to_string();
+    if days != 0 {
+        uptime = format!("{} days, {} hours, {} mins", days, hours, minutes);
+    } else {
+        if hours != 0 {
+            uptime = format!("{} hours, {} mins", hours, minutes);
+        } else {
+            uptime = format!("{} mins", minutes);
+        }
+    }
+    uptime.to_string()
 }
