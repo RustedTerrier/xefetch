f96f0ab5834f0dc85808dc027ad1099475caee5f
RustedTerrier <RustedTerrier@tutanota.com>
2021-04-16 13:56:09 -0400

    Feat: Added faster hostname gathering and removed termion

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-extern crate termion;
-
 use std::{env, fs, process::Command};
 
-use termion::{color, style};
 fn main() {
-    let mut v: Vec<&str> = Vec::new();
-    let distro = get_distro();
+    let mut v: Vec<&str>;
+    let distro = if env::consts::OS == "linux" {
+        get_distro()
+    }
+    else {
+        env::consts::OS.to_string().to_ascii_uppercase()
+    };
     let result = parse_args(&distro);
     let distro_art = match result {
         | Ok(s) => s.to_ascii_uppercase().replace("_", " "),
@@ -40,33 +42,11 @@ fn main() {
     let hme = env::var("HOME").unwrap();
     v = hme.split('/').collect();
     let user = v[v.len() - 1];
-    // Get Host name
-    // MIT License
 
-    // Copyright (c) 2019-2020 The rsfetch contributors
-
-    // Permission is hereby granted, free of charge, to any person obtaining a copy
-    // of this software and associated documentation files (the "Software"), to deal
-    // in the Software without restriction, including without limitation the rights
-    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    // copies of the Software, and to permit persons to whom the Software is
-    // furnished to do so, subject to the following conditions:
-    //
-    // The above copyright notice and this permission notice shall be included in all
-    // copies or substantial portions of the Software.
-    //
-    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    // SOFTWARE.
-
-    let hst = Command::new("hostname")
-        .output()
-        .expect("Could not find hostname.");
-    let host = String::from_utf8(hst.stdout).unwrap().replace("\n", "");
+    // Get hostname
+    let host = fs::read_to_string("/proc/sys/kernel/hostname")
+        .expect("Error: file /proc/sys/kernel/hostname does not exist.")
+        .replace("\n", "");
 
     // Model
     let mdl = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
@@ -104,12 +84,7 @@ fn main() {
     let kernel = kernl.clone();
     let uptime = format_uptime();
     // Get arch
-    let arch = Command::new("uname")
-        .arg("-m")
-        .output()
-        .expect("Make sure you have some form of coreutils installed.");
-    let rch = String::from_utf8(arch.stdout).unwrap().replace("\n", "");
-
+    let arch: String = env::consts::ARCH.to_string();
     // Get CPU
     let comp = fs::read_to_string("/proc/cpuinfo").expect("Error: /proc/cpuinfo does not exist.");
     v = comp.split("\n").collect();
@@ -135,7 +110,7 @@ fn main() {
             model,
             distro,
             distro_art,
-            rch,
+            arch,
             kernel.to_string(),
             uptime,
             shell,
@@ -162,7 +137,8 @@ fn parse_args(distro: &String) -> Result<String, ()> {
         if &c[..] == "-h" || &c[..] == "--help" {
             help = true;
             println!(
-                "Options:\n\
+                "Usage: xefetch [Options]\n\
+                 Options:\n\
                       -h or --help: Prints this message.\n\
                       -l or --logo: Changes the logo art. Ex: xefetch -l void_linux\n"
             );
@@ -260,7 +236,7 @@ fn get_pkgs() -> String {
             let pkgxs: Vec<&str> = pkgsx.split("\n").collect();
             pkg.push(format!("{pgk}(xbps), ", pgk = (pkgxs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
     // APK
     match Command::new("apk").arg("info").output() {
@@ -270,7 +246,7 @@ fn get_pkgs() -> String {
             let pkgas: Vec<&str> = pkgsa.split("\n").collect();
             pkg.push(format!("{pgk}(apk), ", pgk = (pkgas.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // Flatpak
@@ -281,7 +257,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(flatpak), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // Apt
@@ -292,7 +268,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(apt), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // Dnf
@@ -307,7 +283,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(dnf), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // pacman
@@ -322,7 +298,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(pacman), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // portage
@@ -333,7 +309,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(portage), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // Zypper
@@ -352,7 +328,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(zypper), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // nix
@@ -373,7 +349,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(nix), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
 
     // snapd(ew)
@@ -384,7 +360,7 @@ fn get_pkgs() -> String {
             let pkgfs: Vec<&str> = pkgsf.split("\n").collect();
             pkg.push(format!("{pgk}(snapd), ", pgk = (pkgfs.len() - 1)));
         },
-        | Err(why) => {}
+        | Err(_why) => {}
     }
     // Return list
     let mut pkgs: String = pkg.into_iter().collect::<String>();
@@ -399,41 +375,26 @@ fn output(
     kernel: String, uptime: String, shell: String, de: String, cpu: String, pkgs: String
 ) {
     // colors
-    let black = color::Fg(color::Black);
-    let lblack = color::Fg(color::LightBlack);
-    let red = color::Fg(color::Red);
-    let lred = color::Fg(color::LightRed);
-    let green = color::Fg(color::Green);
-    let lgreen = color::Fg(color::LightGreen);
-    let yellow = color::Fg(color::Yellow);
-    let lyellow = color::Fg(color::LightYellow);
-    let blue = color::Fg(color::Blue);
-    let lblue = color::Fg(color::LightBlue);
-    let magenta = color::Fg(color::Magenta);
-    let lmagenta = color::Fg(color::LightMagenta);
-    let cyan = color::Fg(color::Cyan);
-    let lcyan = color::Fg(color::LightCyan);
-    let white = color::Fg(color::White);
-    let lwhite = color::Fg(color::LightWhite);
-    let reset = color::Fg(color::Reset);
-    let bold = style::Bold;
-    let nbold = style::Reset;
+    let black = format!("{}{}", 27 as char, "[38;5;0m");
+    let lblack = format!("{}{}", 27 as char, "[38;5;8m");
+    let red = format!("{}{}", 27 as char, "[38;5;1m");
+    let lred = format!("{}{}", 27 as char, "[38;5;9m");
+    let green = format!("{}{}", 27 as char, "[38;5;2m");
+    let lgreen = format!("{}{}", 27 as char, "[38;5;10m");
+    let yellow = format!("{}{}", 27 as char, "[38;5;3m");
+    let lyellow = format!("{}{}", 27 as char, "[38;5;11m");
+    let blue = format!("{}{}", 27 as char, "[38;5;4m");
+    let lblue = format!("{}{}", 27 as char, "[38;5;12m");
+    let magenta = format!("{}{}", 27 as char, "[38;5;5m");
+    let lmagenta = format!("{}{}", 27 as char, "[38;5;13m");
+    let cyan = format!("{}{}", 27 as char, "[38;5;6m");
+    let lcyan = format!("{}{}", 27 as char, "[38;5;14m");
+    let white = format!("{}{}", 27 as char, "[38;5;7m");
+    let lwhite = format!("{}{}", 27 as char, "[38;5;15m");
+    let reset = format!("{}{}", 27 as char, "[39m");
+    let bold = format!("{}{}", 27 as char, "[1m");
+    let nbold = format!("{}{}", 27 as char, "[0m");
 
-    // Copyright (c) 2018, 2019, 2020 Joe Schillinger <me@schil.li>
-    //
-    // Permission to use, copy, modify, and distribute this software for any
-    // purpose with or without fee is hereby granted, provided that the above
-    // copyright notice and this permission notice appear in all copies.
-    //
-    // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    // WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    // MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    // ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    // WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
-    // println!("{}@{}\n\rOS:     {} {}\n\rHOST:   {}\n\rKERNEL: {}\n\rUPTIME: {}\n\rSHELL:  {}\n\rDE:     {}\n\rCPU:    {}\n\rPKGS:   {}\n\rXEFETCH 1.0\n\r{}██{}██{}██{}██{}██{}██{}██{}██\n{}██{}██{}██{}██{}██{}██{}██{}██{reset}",user,host,distro,arch,model,kernel,uptime,shell,de,cpu,pkgs,black,red,green,yellow,blue,magenta,cyan,white,lblack,lred,lgreen,lyellow,lblue,lmagenta,lcyan,lwhite,reset = color::Fg(color::Reset),);
     match &distro_art[..] {
         | "VOID LINUX" => {
             print!(
@@ -463,6 +424,44 @@ fn output(
                 lcyan,
                 lwhite,
                 lgreen,
+                bold,
+                nbold,
+                reset,
+                pkgs,
+                bold,
+                green,
+                reset,
+                nbold,
+            );
+        },
+        | "ARCH LINUX" => {
+            print!(
+                "{}{}       A         {}{}{}@{}{}{}\n\r      AMA        OS:{}{}     {} {}",
+                bold, blue, user, nbold, reset, bold, blue, host, reset, nbold, distro, arch
+            );
+            print!(
+                "\n\r{}{}     `*<MA       HOST:{}{}   {}\n\r{}{}    AY>,MMA      KERNEL:{}{} {}",
+                bold, blue, nbold, reset, model, bold, blue, nbold, reset, kernel
+            );
+            print!(
+                "\n\r{}{}   AMMY`YMMA     UPTIME:{}{} {}\n\r{}{}  AMM0   0A>*    SHELL:{}{}  {}",
+                bold, blue, nbold, reset, uptime, bold, blue, nbold, reset, shell
+            );
+            print!(
+                "\n\r{}{} AM-*     *-MA   DE:{}{}     {}\n\r{}██{}██{}██{}██{}██{}██{}██{}██{} {}CPU:{}{}    {}",
+                bold, blue, nbold, reset, de, black, red, green, yellow, blue, magenta, cyan, white, blue, bold, nbold, reset, cpu,
+            );
+            print!(
+                "\n\r{}██{}██{}██{}██{}██{}██{}██{}██{} {}PKGS:{}{}   {}\n\r{}{}{}{}",
+                lblack,
+                lred,
+                lgreen,
+                lyellow,
+                lblue,
+                lmagenta,
+                lcyan,
+                lwhite,
+                blue,
                 bold,
                 nbold,
                 reset,
@@ -630,19 +629,42 @@ fn output(
 
         | _ => {
             print!(
-                "{}{}       ___       {}{}{}@{}{}{}\n\r      (.. \\      OS:{}{}     {} {}",
+                "{}{}      ,aa,       {}{}{}@{}{}{}\n\r     ;A00A;      OS:{}{}     {} {}",
                 bold, lwhite, user, nbold, reset, bold, lwhite, host, reset, nbold, distro, arch
             );
             print!(
-                "\n\r{}{}      ({}<>{} |      HOST:{}{}   {}\n\r{}{}     //  \\ \\     KERNEL:{}{} {}",
-                bold, lwhite, yellow, lwhite, nbold, reset, model, bold, lwhite, nbold, reset, kernel
+                "\n\r{}{}     AV{}<>{}VA,     HOST:{}{}   {}\n\r{}{}    AMV  VMA     KERNEL:{}{} {}",
+                bold,
+                lwhite,
+                yellow,
+                lwhite,
+                nbold,
+                reset,
+                model,
+                bold,
+                lwhite,
+                nbold,
+                reset,
+                kernel
             );
             print!(
-                "\n\r{}{}    ( |  | /|    UPTIME:{}{} {}\n\r{}{}   _{}/\\ __)/{}_{})    SHELL:{}{}  {}",
-                bold, lwhite, nbold, reset, uptime, bold, yellow, lwhite, yellow, lwhite, nbold, reset, shell
+                "\n\r{}{}    MV    VM,    UPTIME:{}{} {}\n\r{}{}   J\\{}M;,,;M{}/L,   {}SHELL:{}{}  {}",
+                bold,
+                lwhite,
+                nbold,
+                reset,
+                uptime,
+                bold,
+                yellow,
+                lwhite,
+                yellow,
+                lwhite,
+                nbold,
+                reset,
+                shell
             );
             print!(
-                "\n\r{}{}   \\/{}-____{}\\/{}     DE:{}{}     {}\n\r{}██{}██{}██{}██{}██{}██{}██{}██{} {}CPU:{}{}    {}",
+                "\n\r{}{}   VMV{}\"\"\"\"{}VMV    {}DE:{}{}     {}\n\r{}██{}██{}██{}██{}██{}██{}██{}██{} {}CPU:{}{}    {}",
                 bold, yellow, lwhite, yellow, lwhite, nbold, reset, de, black, red, green, yellow, blue, magenta, cyan, white, lwhite, bold, nbold, reset, cpu,
             );
             print!(
